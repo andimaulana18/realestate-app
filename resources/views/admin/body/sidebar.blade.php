35edadd10024a5b185818dda65fd7b11ddbac156
andimaulana18 <andimaulanayusup12@gmail.com>
2024-03-02 13:55:57 +0700

    first commit

---
+++ resources/views/admin/body/sidebar.blade.php
@@ -0,0 +1,44 @@
+<!-- Sidebar Start -->
+<nav class="sidebar">
+    <div class="sidebar-header">
+      <a href="#" class="sidebar-brand">
+        Learning<span>Login</span>
+      </a>
+      <div class="sidebar-toggler not-active">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+    <div class="sidebar-body">
+      <ul class="nav">
+        <li class="nav-item nav-category">Main</li>
+        <li class="nav-item">
+          <a href="{{ route('admin.dashboard') }}" class="nav-link">
+            <i class="link-icon" data-feather="box"></i>
+            <span class="link-title">Dashboard</span>
+          </a>
+        </li>
+        <li class="nav-item nav-category">web apps</li>
+        <li class="nav-item">
+          <a class="nav-link" href="#emails">
+            <i class="link-icon" data-feather="mail"></i>
+            <span class="link-title">Email</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="pages/apps/chat.html" class="nav-link">
+            <i class="link-icon" data-feather="message-square"></i>
+            <span class="link-title">Chat</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="pages/apps/calendar.html" class="nav-link">
+            <i class="link-icon" data-feather="calendar"></i>
+            <span class="link-title">Calendar</span>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+  <!-- Sidebar End -->
